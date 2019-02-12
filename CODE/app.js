ea81b44124883c1d0ccf32f4f65ec4fd7aea5acf
Manuel Mosca <mosca.98.05@gmail.com>
2019-02-12 10:39:05 +0100

    implement wireframe disign

--- CODE/app.js
+++ CODE/app.js
@@ -1,8 +1,23 @@
-init = function()
-  {
-    console.log("init!");
-    $("title").text("Dettaglio Drone");
+app = {
+  init : function() {
+      console.log("init inside app!");
+      $("title").text("New title");
+      $.get("data.json")
+      .done(app.onSuccess)
+      .fail(app.onError);
+      
+  },
+  onSuccess:function(jsonData){
+      // processa i dati qui!!!
+      console.log(jsonData);
+  },
+  
+  onError:function(e){
+      console.log("error!");
+      console.log(JSON.stringify(e));
   }
+};
 
 
-$(document).ready(init)
+
+$(document).ready(app.init);
