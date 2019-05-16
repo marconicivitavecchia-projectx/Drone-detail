e01641548ba4fdc7e80c457d924f7b60cdf571a6
Manuel Mosca <mosca.98.05@gmail.com>
2019-05-16 12:00:44 +0200

    dinamicize

--- CODE/app.js
+++ CODE/app.js
@@ -2,7 +2,7 @@ app = {
   init : function() {
       console.log("init inside app!");
       $("title").text("New title");
-      $.getJSON("data.json")
+      $.getJSON("https://projectx-marconi.firebaseio.com/dronedetail.json")
       .done(app.onSuccess)
       .fail(app.onError);
       
@@ -10,6 +10,10 @@ app = {
   onSuccess:function(jsonData){
       // processa i dati qui!!!
       console.log(jsonData);
+      $("#stanza").text(jsonData.stanza)
+      $("#oggetto").text(jsonData.oggetto)
+      $("#connessione").text(jsonData.connessione)
+      $("#batteria").text(jsonData.batteria)
   },
   
   onError:function(e){
