705e2426d62fc36240e671c1b3e8cc4708bac167
wbigger <wbigger@gmail.com>
2019-05-22 06:01:04 +0200

    Add support for firebase

--- CODE/app.js
+++ CODE/app.js
@@ -1,25 +1,48 @@
 app = {
-  init : function() {
-      console.log("init inside app!");
-      $("title").text("New title");
-      $.getJSON("https://projectx-marconi.firebaseio.com/dronedetail.json")
-      .done(app.onSuccess)
-      .fail(app.onError);
-      
-  },
-  onSuccess:function(jsonData){
-      // processa i dati qui!!!
-      console.log(jsonData);
-      $("#stanza").text(jsonData.stanza)
-      $("#oggetto").text(jsonData.oggetto)
-      $("#connessione").text(jsonData.connessione)
-      $("#batteria").text(jsonData.batteria)
-  },
-  
-  onError:function(e){
-      console.log("error!");
-      console.log(JSON.stringify(e));
-  }
+    droneLocation: "",
+    init: function () {
+        console.log("init inside app!");
+        $("title").text("New title");
+        $.getJSON("https://projectx-marconi.firebaseio.com/dronedetail.json")
+            .done(app.onSuccess)
+            .fail(app.onError);
+        $("#go-button").on("click", app.go);
+    },
+    go: function () {
+        let location = $("#stanza").text();
+        console.log(location);
+        if (location === "salotto") {
+            location = "camera";
+        } else {
+            location = "salotto";
+        }
+        let jsonData = JSON.stringify(location);
+        console.log(jsonData);
+        $.ajax({
+            type: "PUT",
+            url: "https://projectx-marconi.firebaseio.com/dronedetail/stanza.json",
+            data: jsonData,
+            contentType: "application/json",
+        }).done(app.onGoSuccess)
+            .fail(app.onError);
+    },
+    onGoSuccess: function (jsonData) {
+        console.log(`onGoSuccess: ${jsonData}`)
+        $("#stanza").text(jsonData);
+    },
+    onSuccess: function (jsonData) {
+        // processa i dati qui!!!
+        console.log(jsonData);
+        $("#stanza").text(jsonData.stanza)
+        $("#oggetto").text(jsonData.oggetto)
+        $("#connessione").text(jsonData.connessione)
+        $("#batteria").text(jsonData.batteria)
+    },
+
+    onError: function (e) {
+        console.log("error!");
+        console.log(JSON.stringify(e));
+    }
 };
 
 
